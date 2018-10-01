74117d01547ea67a969466b79a48ed48cda81a77
KiwiTech <kiwitech@kt-mac-179.local>
2018-10-01 11:55:50 +0530

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by KiwiTech on 01/10/18.
+//  Copyright © 2018 KiwiTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
